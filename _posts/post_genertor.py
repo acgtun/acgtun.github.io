a5e8ad7ee34c82fb3735c03fd43b76e49322860d
haifeng <chenhaifeng88888@gmail.com>
2017-12-17 18:04:40 -0800

    add leetcode page

---
+++ _posts/post_genertor.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import time
+from os import walk
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def write_post(problem, lang, content, create_time):
+    create_time = time.gmtime(create_time)
+    file_name = time.strftime("%Y-%m-%d-%H-%M-%S", create_time)
+    file = open(file_name + '-' + problem.replace(' ', '-') + '.html', 'w')
+    file.write('---\n')
+    file.write('layout: post\n')
+    file.write('title: ' + problem + '\n')
+    file.write('date: ' + time.strftime("%Y-%m-%d %H:%M:%S", create_time) + '\n')
+    file.write('categories: leetcode\n')
+    file.write('---\n\n')
+    file.write('{ % raw %}\n\n')
+    file.write('{% highlight ' + lang + ' %}\n\n')
+
+    file.write(content + '\n\n')
+
+    file.write('{% endhighlight %}\n')
+    file.write('{ % endraw %}\n\n')
+    file.close()
+
+
+def get_solutions():
+    langs = ['cpp', 'java', 'python']
+    path = os.path.join(os.path.expanduser('~/gitcode/leetcode/algorithms/'))
+    print('path: {}'.format(path))
+    problems = {}
+    for lang in langs:
+        dir = os.path.join(path, lang)
+        for dirpath, dirnames, filenames in walk(dir):
+            for file in filenames:
+                problem = file.replace('.' + lang, '')
+                if problem not in problems.keys():
+                    problems[problem] = {}
+                file_path = os.path.join(dir, file)
+                with open(file_path, 'r') as content_file:
+                    content = content_file.read()
+
+                write_post(problem, lang, content, os.path.getmtime(file_path))
+
+
+if __name__ == '__main__':
+    get_solutions()
